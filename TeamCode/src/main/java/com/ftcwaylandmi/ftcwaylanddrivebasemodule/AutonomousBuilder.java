805a2a510a85ee6bd20e646a332df472a21d6f51
AlexFirstRobotics <crossalex@waylandunion.org>
2022-11-02 13:30:30 -0400

    Auton

--- TeamCode/src/main/java/com/ftcwaylandmi/ftcwaylanddrivebasemodule/AutonomousBuilder.java
+++ TeamCode/src/main/java/com/ftcwaylandmi/ftcwaylanddrivebasemodule/AutonomousBuilder.java
@@ -1,23 +1,42 @@
 package com.ftcwaylandmi.ftcwaylanddrivebasemodule;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class AutonomousBuilder
 {
 
     /* Called Classes */
-    FtcWaylandDrivebaseModule ftcWaylandDrivebaseModule;
+    private FtcWaylandDrivebaseModule ftcWaylandDrivebaseModule;
+    private Config config;
 
     private DcMotor moduleA = ftcWaylandDrivebaseModule.moduleA;
     private DcMotor moduleB = ftcWaylandDrivebaseModule.moduleB;
 
-    public void CreateAutonomous (
+    private final String autonName = config.AutonName;
+    private final String autonGroup = config.AutonGroup;
+
+    public void CreateAutonRearDrive (
 
     )
     {
+        @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = autonName, group = autonGroup)
+        abstract class Auton extends LinearOpMode
+        {
+
+            public void runOpMode()
+            {
+
+
+
+            }
+
+        }
         moduleA.setMode(RUN_TO_POSITION);
         moduleB.setMode(RUN_TO_POSITION);
     }
+
 
 }
--- TeamCode/src/main/java/com/ftcwaylandmi/ftcwaylanddrivebasemodule/Config.java
+++ TeamCode/src/main/java/com/ftcwaylandmi/ftcwaylanddrivebasemodule/Config.java
@@ -6,4 +6,7 @@ public class Config
     public static final String  TeleOpName = "TeleOp";
     public static final String TeleOpGroup = "11846";
 
+    public static final String AutonName = "AutonTest";
+    public static final String AutonGroup = "11846";
+
 }
